a6a6a388adfbb0175376177d582e30b040c668b8
Harper Mascarenhas <105064849+harperbolic@users.noreply.github.com>
2023-10-06 22:59:00 -0300

    final day commit

--- unity/Assets/Scripts/levelSpawner.cs
+++ unity/Assets/Scripts/levelSpawner.cs
@@ -2,7 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Level1 : MonoBehaviour
+public class levelSpawner : MonoBehaviour
 {
-    
+
+	[SerializeField] private Transform level;
+	private void Awake() {
+		Instantiate(level, new Vector3(0,0), Quaternion.identity);
+		
+
+	}
+
 }
