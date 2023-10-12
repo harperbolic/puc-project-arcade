09981293743eb80522ef34b35f291996631936bb
Harper Mascarenhas <git@harper.rocks>
2023-10-12 20:03:27 -0300

    fazer menu pause e settings

--- unity/Assets/Scripts/UI/PauseControl.cs
+++ unity/Assets/Scripts/UI/PauseControl.cs
@@ -6,16 +6,20 @@ public class PauseControl : MonoBehaviour
 {
 
     public static bool gameIsPaused;
+    [SerializeField] private GameObject pauseMenu;
+
+    void Start()
+    {
+    
+    }
 
     void Update()
     {
 
 	if (Input.GetButtonDown("Pause"))
 	{
-	
 		gameIsPaused = !gameIsPaused;
-		PauseGame();
-		
+		PauseGame();	
 	}
 
     }
@@ -25,10 +29,14 @@ public class PauseControl : MonoBehaviour
         if(gameIsPaused)
         {
             Time.timeScale = 0f;
+
+	    pauseMenu.SetActive(true);
         }
         else 
         {
             Time.timeScale = 1;
+
+	    pauseMenu.SetActive(false);
         }
     }
 }
