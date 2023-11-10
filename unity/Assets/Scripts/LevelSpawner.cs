ba18cc31a5f10372b2b7acc3954e3b8fa48fb8df
LauraLob0 <122253860+LauraLob0@users.noreply.github.com>
2023-11-10 18:20:22 -0300

    Conserta a estrutura das fases

--- unity/Assets/Scripts/LevelSpawner.cs
+++ unity/Assets/Scripts/LevelSpawner.cs
@@ -7,27 +7,30 @@ public class LevelSpawner : MonoBehaviour
 {
 	[SerializeField]
 	private float secondsToStart;
-	[SerializeField] private Level level;
+	[SerializeField] private Level[] levels;
+	private int currentLevelIndex;
+	private Level currentLevel;
 	public GameObject[] spawnPoints;
 
 	[SerializeField] private Player player;
 	private void Start()
 	{
-		StartCoroutine(Spawn());
+		currentLevelIndex = -1;
+		SetCurrentLevel();
 	}
 
 	private IEnumerator Spawn()
 	{
 		//Logar quantos segundos dura a seção da fase (útil para evitar manter a fase curta ou longa demais)
 		float levelLength = secondsToStart;
-		foreach (var row in level.rows)
+		foreach (var row in currentLevel.rows)
 		{
 			levelLength += row.secondsToNextRow;
 		}
 		Debug.Log("Essa seção durará um total de: " + (levelLength) + " segundos");
 		yield return new WaitForSeconds(secondsToStart);
 		
-		foreach (var row in level.rows)
+		foreach (var row in currentLevel.rows)
 		{
 			foreach (var toSpawn in row.spawnPoints)
 			{
@@ -40,11 +43,25 @@ public class LevelSpawner : MonoBehaviour
 			}
 			yield return new WaitForSeconds(row.secondsToNextRow);
 		}
-		//TODO TROCAR SEÇÕES DO LEVEL - Fazer para a etapa 3
-		SceneManager.LoadScene("Victory");
+
+		if (currentLevelIndex + 1 == levels.Length)
+		{
+			SceneManager.LoadScene("Victory");
+		}
+		else
+		{
+			SetCurrentLevel();
+		}
 	}
 	public float GetSpawnPoint (int s)
 	{
 		return spawnPoints[s].transform.position.x;
+	}
+
+	private void SetCurrentLevel()
+	{
+		currentLevelIndex++;
+		currentLevel = levels[currentLevelIndex];
+		StartCoroutine(Spawn());		
 	}
 }
